aff59ba86c2f788de4443e191be1aa34d8a4e061
Prakhar Singh <singhprakhar359@gmail.com>
2024-06-18 16:00:31 +0530

    improved visibility

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,51 +1,99 @@
-import {React,useContext} from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import { React, useContext,useCallback,useEffect,useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import homeimg from './assets/img1.png';
 import alertContext from "../context/Alert/alertContext";
 import { useGoogleLogin } from '@react-oauth/google';
 
+
 const Home = () => {
+
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(200 - Math.random() * 100);
+    const period = 900;
+
+    const tick = useCallback(() => {
+        const toRotate = ["Notes", "WebPage","URLs"];
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting
+            ? fullText.substring(0, text.length - 1)
+            : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(prevLoopNum => prevLoopNum + 1);
+            setDelta(300);
+        }
+    }, [isDeleting, loopNum, text]);
+
+    useEffect(() => {
+        let ticker = setInterval(tick, delta);
+
+        return () => {
+            clearInterval(ticker);
+        };
+    }, [tick, delta]);
+
 
     const { showAlert } = useContext(alertContext);
     let navigate = useNavigate();
 
     const login = useGoogleLogin({
         onSuccess: (tokenResponse) => {
-        if (tokenResponse) {
-            // If login is successful, save authToken to local storage and redirect to home page
-            localStorage.setItem('token', tokenResponse);
-            navigate('/');
-            // Show success alert
-            showAlert('Welcome back! Successfully Loggedin :)', 'success')
+            if (tokenResponse) {
+                // If login is successful, save authToken to local storage and redirect to home page
+                localStorage.setItem('token', tokenResponse);
+                navigate('/');
+                // Show success alert
+                showAlert('Welcome back! Successfully Loggedin :)', 'success')
+            }
+            else {
+                // If login is unsuccessful, show warning alert
+                showAlert('Inavalid Credentials! Please Login Using Correct Credentials.', 'warning');
+            }
         }
-        else {
-            // If login is unsuccessful, show warning alert
-            showAlert('Inavalid Credentials! Please Login Using Correct Credentials.', 'warning');
-        }
-    }
-      });
-       //https://www.googleapis.com/auth/userinfo.email
+    });
+    //https://www.googleapis.com/auth/userinfo.email
     return (
         <>
             {/* Hero Section */}
-            <div className="container my-6">
-              
+            <div className="container my-6" >
+
                 <div className="row">
 
-                  <div className="col-md-5">
+                    <div className="col-md-5">
                         <img className="img-fluid " src={homeimg} alt='home' />
                     </div>
 
                     <div className="col-md-7 my- 3 d-flex flex-column justify-content-center">
                         <div className="px-5 py-3 my-3 align-middle">
-                            <h1 className='display-3 my-3 ' style={{ fontWeight: "revert-layer" }}>SummarizeIt📝</h1>
+                            <h1 className='display-4 my-3 ' style={{ fontWeight: "revert-layer" }}>
+                                {`Summarize,`}{" "}
+                                <span
+                                    className="txt-rotate"
+                                    data-rotate='["Notes", "WebPage","URLs"]'
+                                >
+                                    <span className="wrap">{text}</span>
+                                </span>
+                            </h1>
                             <h3>An AI-Powered Summary <span style={{ color: "darkred", fontWeight: "bold" }}> Generation App</span> </h3>
                             <p>
                                 {" "}
                                 Introducing AI-Powered Platform that extracts
-                                summaries seamlessly from web URLs or manual input, into concise summaries, which 
+                                summaries seamlessly from web URLs or manual input, into concise summaries, which
                                 users can easily save for future reference and use.
-                           </p>
+                            </p>
                         </div>
 
                         {/* Show Login and SignUp buttons only if the user is not authenticated */}
@@ -53,7 +101,7 @@ const Home = () => {
                             <Link className="btn btn-lg btn-outline-primary me-3 my-2" to="/login" role="button" >Login</Link>
                             <Link className="btn btn-lg btn-outline-primary me-3 my-2" to="/signup" role="button" >SignUp For Free</Link>
                             <button className="btn btn-lg btn-outline-primary my-2" onClick={() => login()}>
-                                SignUp with <i className="fab fa-google fa-x"/></button>
+                                SignUp with <i className="fab fa-google fa-x" /></button>
                         </div>
                         }
 
@@ -64,7 +112,7 @@ const Home = () => {
                         }
                     </div>
 
-                  
+
                 </div>
             </div>
 
